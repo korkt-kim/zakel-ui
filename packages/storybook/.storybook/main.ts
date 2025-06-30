7972cdabe05cc249e1b2b347160395d6b6ee9549
korkt-kim <gnscott6@naver.com>
2025-06-30 22:39:08 +0900

    feat: Text Storybook 생성

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,6 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from "vite";
-import KumaUI from "@zakel-ui/vite";
 import { join, dirname } from "path"
 
 /**
--- packages/storybook/src/stories/Heading.tsx
+++ packages/storybook/src/stories/Heading.tsx
@@ -1,6 +1,7 @@
 import { Heading as _Heading, type HeadingComponent } from "@zakel-ui/core";
 import type { ComponentType } from "react";
 
+// @MEMO: 라이브러리에서 HeadingProps 잘못된것 확인
 export const Heading = (props: Parameters<HeadingComponent>[0]) => {
   const HeadingComponent = _Heading as ComponentType<Parameters<HeadingComponent>[0]>;
 
